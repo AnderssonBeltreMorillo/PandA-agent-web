e36086364836c09c6f9c692613250b9e38bcfae4
Andersson Beltre <20210853@itla.edu.do>
2026-09-18 19:57:00 -0400

    Actualiza firebase.js con credenciales reales

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXCnmxasZalPp7s_RjMfLfGl8fnJzIN-4",
+  authDomain: "pya-agent.firebaseapp.com",
+  projectId: "pya-agent",
+  storageBucket: "pya-agent.firebasestorage.app",
+  messagingSenderId: "551652468951",
+  appId: "1:551652468951:web:527aac624ebda1c9616892",
+  measurementId: "G-6QRL5F66FT"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
+export { signInWithPopup, signOut };
